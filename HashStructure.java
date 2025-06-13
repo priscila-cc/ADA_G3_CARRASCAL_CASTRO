34ef8c32d3156a42d5e0c2a6d5345b3613186a75
priscila-cc <104471308+priscila-cc@users.noreply.github.com>
2025-06-12 23:38:58 -0500

    Delete HashStructure.java

--- HashStructure.java
+++
@@ -1,7 +0,0 @@
-package paquete_principal;
-
-public interface HashStructure {
-    void addEntry(String lookupKey, ClientData dataValue);
-    ClientData findEntry(String searchKey);
-    long getLastOpDuration(); // Para registrar la duración de la última operación
-}
